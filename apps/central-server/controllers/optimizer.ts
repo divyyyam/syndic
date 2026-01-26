e6909718ea7cf6d4a9519a6bd7e332641e24c246
Divyam Singh <divyamsingh1918@gmail.com>
2026-01-27 02:59:27 +0530

    updates

--- apps/central-server/controllers/optimizer.ts
+++
@@ -1,1 +0,0 @@
-//initially gemini api / graph logic
--- apps/central-server/routes/cryptotofiat.route.ts
+++
@@ -1,9 +0,0 @@
-import express,{Request,Response} from "express"
-
-
-const cryptoToFiatRouter = express.Router()
-
-
-
-
-export default cryptoToFiatRouter
